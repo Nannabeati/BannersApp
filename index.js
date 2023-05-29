0095c7e17f778042a8a6abd9516e40355d2b66e3
Nannabeati <134772330+Nannabeati@users.noreply.github.com>
2023-05-29 11:43:46 +0530

    fixed errors in index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 // Write your code here.
-import './index.js'
+import './index.css'
+
 const BannerCardItem = props => {
   const {bannerElements} = props
   const {headerText, className, description} = bannerElements
   const cardContainer = `card-container ${className}`
-  return {
-    <li className="cardContainer">
+  return (
+    <li className={cardContainer}>
       <h1 className="heading">{headerText}</h1>
       <p className="para">{description}</p>
       <div>
-        <button className="button">"Show More"</button>
+        <button type="button" className="button">
+          Show More
+        </button>
       </div>
     </li>
-  }
+  )
 }
+
+export default BannerCardItem
